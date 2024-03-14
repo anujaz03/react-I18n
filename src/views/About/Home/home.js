a71524d2b51a9099969485e1dc0081fdbaffe3fc
anuja_z03 <anujazade18@gmail.com>
2024-03-14 23:31:17 +0530

    changes

--- src/views/About/Home/home.js
+++ src/views/About/Home/home.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, {  useState } from 'react';
 import "./home.css"
+import I18n from '../../../utilities/I18n';
 
 function home() {
+
+    //const usersCount =15;
+
+
   return (
     <div>
-      Home
+      <h1>{I18n("welcomeMessage")}</h1>
+      <p>
+        {I18n("normalMessage")}
+      </p>
+      <h3>{I18n("greetingMessage")}</h3>
+      <select
+      defaultValue={localStorage.getItem("lang")} onChange={(e)=>{
+        localStorage.setItem("lang",e.target.value);
+        window.location.reload();
+      }}>
+        <option value ="en">English</option>
+        <option value ="hi">Hindi</option>
+        <option value ="mr">Marathi</option>
+      </select>
+    {/*<>p >
+        {usersCount} users are learning in this session.
+  </p >*/}
     </div>
   )
 }
